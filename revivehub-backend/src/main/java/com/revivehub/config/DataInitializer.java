b8045c2236369498446ffbdfa9c539b1150ffcaa
Diwakar <the.walkersingh@hotmail.com>
2025-03-19 23:46:48 +0530

    Latest

--- revivehub-backend/src/main/java/com/revivehub/config/DataInitializer.java
+++ revivehub-backend/src/main/java/com/revivehub/config/DataInitializer.java
@@ -7,28 +7,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
-  private final RoleRepository roleRepository;
-  
-  public DataInitializer(RoleRepository roleRepository) {
-	this.roleRepository = roleRepository;
-  }
-  
-  @Override
-  public void run(String... args) throws Exception {
-	// Check and insert ROLE_USER
-	if (roleRepository.findByName(RoleType.ROLE_USER).isEmpty()) {
-	  Role userRole = new Role();
-	  userRole.setName(RoleType.ROLE_USER);
-	  roleRepository.save(userRole);
-	  System.out.println("ROLE_USER created.");
-	}
-	
-	// Check and insert ROLE_ADMIN
-	if (roleRepository.findByName(RoleType.ROLE_ADMIN).isEmpty()) {
-	  Role adminRole = new Role();
-	  adminRole.setName(RoleType.ROLE_ADMIN);
-	  roleRepository.save(adminRole);
-	  System.out.println("ROLE_ADMIN created.");
-	}
-  }
+   private final RoleRepository roleRepository;
+   
+   public DataInitializer(RoleRepository roleRepository) {
+	  this.roleRepository = roleRepository;
+   }
+   
+   @Override public void run(String... args) throws Exception {
+	  // Check and insert ROLE_USER
+	  if (roleRepository.findByName(RoleType.ROLE_USER).isEmpty()) {
+		 Role userRole = new Role();
+		 userRole.setName(RoleType.ROLE_USER);
+		 roleRepository.save(userRole);
+		 System.out.println("ROLE_USER created.");
+	  }
+	  
+	  // Check and insert ROLE_ADMIN
+	  if (roleRepository.findByName(RoleType.ROLE_ADMIN).isEmpty()) {
+		 Role adminRole = new Role();
+		 adminRole.setName(RoleType.ROLE_ADMIN);
+		 roleRepository.save(adminRole);
+		 System.out.println("ROLE_ADMIN created.");
+	  }
+   }
 }
